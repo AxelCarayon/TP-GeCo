21e0150aeaec50645dec92a1f78886d775191697
AxelCarayon <axel.carayon@gmail.com>
2020-11-16 11:58:34 +0100

    #2 ajout testLoginForNomAndPrenom

---
+++ src/test/java/geco/LoginGeneratorTest.java
@@ -0,0 +1,33 @@
+package geco;
+
+import jdk.nashorn.internal.runtime.ECMAException;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class LoginGeneratorTest extends TestCase {
+
+    LoginService ls;
+    LoginGenerator lg;
+
+    @Before
+    public void setUp() throws Exception {
+        ls = new LoginService(new String[]{"JROL","BPER", "CGUR", "JDU", "JRAL", "JRAL1"});;
+        lg = new LoginGenerator(ls);
+    }
+
+    @Test
+    public void testLoginForNomAndPrenom() throws Exception {
+        assertEquals("PDUR",lg.generateLoginForNomAndPrenom("Durand","Paul"));
+    }
+
+    @After
+    public void tearDown() throws Exception {
+    }
+}
