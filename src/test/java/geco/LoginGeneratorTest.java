38294cb05ca8153bbd987426ec1ab601942d9dc2
AxelCarayon <axel.carayon@gmail.com>
2020-11-30 10:23:32 +0100

    version 1.0.1

--- src/test/java/geco/LoginGeneratorTest.java
+++ src/test/java/geco/LoginGeneratorTest.java
@@ -18,19 +18,25 @@ public class LoginGeneratorTest extends TestCase {
 
     @Before
     public void setUp() throws Exception {
-        ls = new LoginService(new String[]{"JROL","BPER", "CGUR", "JDU", "JRAL", "JRAL1"});;
+        ls = new LoginService(new String[]{"JROL", "BPER", "CGUR", "JDU", "JRAL", "JRAL1"});
+        ;
         lg = new LoginGenerator(ls);
     }
 
     @Test
     public void testLoginForNomAndPrenom() throws Exception {
-        assertEquals("PDUR",lg.generateLoginForNomAndPrenom("Durand","Paul"));
-        assertEquals("JROL1",lg.generateLoginForNomAndPrenom("Rolling","Jean"));
+        assertEquals("PDUR", lg.generateLoginForNomAndPrenom("Durand", "Paul"));
+        assertEquals("JROL1", lg.generateLoginForNomAndPrenom("Rolling", "Jean"));
     }
 
     @Test
     public void testIteration() throws Exception {
-        assertEquals("JRAL2",lg.generateLoginForNomAndPrenom("Ralling","John"));
+        assertEquals("JRAL2", lg.generateLoginForNomAndPrenom("Ralling", "John"));
+    }
+
+    @Test
+    public void testShortName() throws Exception{
+        assertEquals("PDU",lg.generateLoginForNomAndPrenom("Du","Paul"));
     }
 
     @After
